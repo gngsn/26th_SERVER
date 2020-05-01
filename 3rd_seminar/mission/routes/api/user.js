d9095d70f53254da479b27beef606e17a75e93d2
gngsn <rudtjs4540@gmail.com>
2020-05-01 22:09:59 +0900

    update 3rd_seminar

--- 3rd_seminar/mission/routes/api/user.js
+++ 3rd_seminar/mission/routes/api/user.js
@@ -13,30 +13,6 @@ let resMessage = require('../../modules/responseMessage');
     RESPONSE STATUS : 200 (OK)
     RESPONSE DATA : User ID
 */
-
-// 1단계
-// router.post('/signup', async (req, res) => {
-//     const { id, name, password, email } = req.body;
-//     User.push({id, name, password, email});
-//     res.status(200).send(User);
-// });
-
-// 2단계
-// router.post('/signup', async (req, res) => {
-//     const { id, name, password, email } = req.body;
-//     // request data 확인 - 없다면 Bad Request 반환
-//     if ( !id || !name || !password || !email ) {
-//         return res.status(400).send({ message: 'BAD REQUEST' });
-//     }
-//     //already ID
-//     if (User.filter(user => user.id == id).length > 0) {
-//         return res.status(400).send({ message: 'ALREADY ID' });
-//     }
-//     User.push({id, name, password, email});
-//     res.status(200).send(User);
-// });
-
-// 3단계
 router.post('/signup', async (req, res) => {
     const {
         id,
@@ -44,7 +20,7 @@ router.post('/signup', async (req, res) => {
         password,
         email
     } = req.body;
-    // request data 확인 - 없다면 Null Value 반환
+    // request data 확인 - 없다면 Bad Request 반환
     if (!id || !name || !password || !email) {
         res.status(statusCode.BAD_REQUEST)
             .send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
@@ -63,25 +39,52 @@ router.post('/signup', async (req, res) => {
         email
     });
     res.status(statusCode.OK)
-        .send(util.success(statusCode.OK, resMessage.CREATED_USER, {
-            userId: id
-        }));
+        .send(util.success(statusCode.OK, resMessage.CREATED_USER, {userId: id}));
 });
 
 /* 
     ✔️ sign in
     METHOD : POST
     URI : localhost:3000/api/user/signin
-    REQUEST BODY : id, password
+    REQUEST BODY : id, name
     RESPONSE STATUS : 200 (OK)
     RESPONSE DATA : User ID
 */
 router.post('/signin', async (req, res) => {
     // request body 에서 데이터 가져오기
+    const {
+        id,
+        password
+    } = req.body;
     // request data 확인 - 없다면 Bad Request 반환
+    if (!id || !password) {
+        res.status(statusCode.BAD_REQUEST)
+            .send(util.fail(statusCode.BAD_REQUEST, statusCode.BAD_REQUEST));
+        return;
+    }
+    const user = User.filter(user => user.id == id);
     // 존재하는 아이디인지 확인 - 없다면 No user 반환
+    if (user.length == 0) {
+        res.status(statusCode.BAD_REQUEST)
+            .send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
+        return;
+    }
     // 비밀번호 확인 - 없다면 Miss match password 반환
+    if (user[0].password !== password ) {
+        res.status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, resMessage.MISS_MATCH_PW));
+        return;
+    }
     // 성공 - login success와 함께 user Id 반환
+    res.status(statusCode.OK)
+        .send(util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, {userId: id}));
 });
+
+
+router.get('/profile/:id', async (req, res) => {
+    console.log(req.params.id);
+});
+
+
 
 module.exports = router;
---
+++ 3rd_seminar/mission/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.use('/api', require('./api'));
+
+module.exports = router;
