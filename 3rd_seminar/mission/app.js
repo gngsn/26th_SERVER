0c856661fb20a90d6bd0c5705ce94067ed375925
gngsn <rudtjs4540@gmail.com>
2020-05-03 01:31:37 +0900

    remove unnecessary dir & files

--- 3rd_seminar/mission/app.js
+++ 3rd_seminar/mission/app.js
@@ -5,12 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
