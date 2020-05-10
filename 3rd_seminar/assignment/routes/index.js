8f0c2eb722a6369348ab2bf62a5ad8207aa48437
gngsn <rudtjs4540@gmail.com>
2020-05-10 22:55:12 +0900

    create 3rd assignment ex

---
+++ 3rd_seminar/assignment/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.use('/post', require('./post'));
+
+module.exports = router;
---
+++ 3rd_seminar/assignment/routes/post.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', async (req, res) => {
+    r
+});
+
+module.exports = router;
