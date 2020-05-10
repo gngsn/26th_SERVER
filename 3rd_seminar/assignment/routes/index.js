7bafbcf837b8db57747e04c0a7c85fec338212ec
gngsn <rudtjs4540@gmail.com>
2020-05-11 02:07:11 +0900

    update assignment

--- 3rd_seminar/assignment/routes/index.js
+++ 3rd_seminar/assignment/routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 router.use('/post', require('./post'));
 
 module.exports = router;
